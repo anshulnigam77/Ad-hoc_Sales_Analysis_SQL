dab4383d575c90778a285105a847d26dde4fab23
Anshul Nigam <160848154+anshulnigam77@users.noreply.github.com>
2024-06-30 12:16:26 +0530

    Delete AtliQ Sales Analytics directory

--- AtliQ Sales Analytics/Metrics by Rank/1.Top 5 Customers (2021).sql
+++
@@ -1,7 +0,0 @@
-SELECT
-dc.customer, ROUND(SUM(net_sales)/1000000,2) as net_sales_mln
-FROM net_sales ns
-JOIN dim_customer dc ON ns.customer_code = dc.customer_code
-WHERE fiscal_year = 2021
-GROUP BY customer
-ORDER BY net_sales_mln DESC LIMIT 5
--- AtliQ Sales Analytics/Metrics by Rank/3.Top 5 Products (2021).sql
+++
@@ -1,6 +0,0 @@
-SELECT
-product, ROUND(SUM(net_sales)/1000000,2) as net_sales_mln
-FROM net_sales
-WHERE fiscal_year = 2021
-GROUP BY product
-ORDER BY net_sales_mln DESC LIMIT 5
